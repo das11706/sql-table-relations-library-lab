e3ea86f483aa475f4e2f19f72d1c785b5f739d7c
Daniel Suarez <suarez.daniel101@gmail.com>
2021-08-16 21:54:32 -0400

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,14 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  INNER JOIN characters
+  ON series.author_id = characters.author_id
+  GROUP BY series.title
+  HAVING species = MAX(species)
+  ORDER BY series.title
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
